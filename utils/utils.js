b44843a503ea489bc45a17c27bda3ec9ab52e80d
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-12-31 14:58:46 -0600

    added deploy contract code

--- utils/utils.js
+++ utils/utils.js
@@ -9,6 +9,8 @@ const pinata = new pinataSDK(
   process.env.pinata_api_key,
   process.env.pinata_secret_api_key
 ); // authenticates when using 'pinata'
+import pkg from "hardhat";
+const { hre, ethers, run, network } = pkg;
 
 ("use strict");
 
@@ -25,6 +27,7 @@ export async function completeEntireSequence() {
     // pinImagesAndAnims,
     // buildFinalMetaAndPinToIPFS,
     // buildProjectMetaAndPinToIPFS,
+    deployContract,
     // buildScriptsForDatabase,
   ];
   for (const fn of seq) await fn().then(console.log("Completing " + fn.name));
@@ -311,7 +314,99 @@ ${projectInfo.royaltiesPercent},
   );
 }
 
-export async function deployContract() {}
+export async function deployContract() {
+  deploy().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+}
+
+async function deploy() {
+  const args = {
+    mint_price: "20000000000000000", // .02 MATIC
+    max_tokens: 3,
+    base_uri:
+      "https://ipfs.io/ipfs/QmTwiGMeNjhrECN5tHkSEN7jHDEQ3tvFzeCXF4f3EhZJzv",
+    royaltyArtist: process.env.STUNT_WALLET_ADDRESS,
+    royaltyBasis: 500,
+  };
+  const courseNFTContractFactory = await ethers.getContractFactory(
+    "CourseNFTContract"
+  );
+  const courseNFTContract = await courseNFTContractFactory.deploy(
+    args.mint_price,
+    args.max_tokens,
+    args.base_uri,
+    args.royaltyArtist,
+    args.royaltyBasis
+  );
+  // deploy
+  console.log("Deploying...");
+  await courseNFTContract.waitForDeployment(
+    args.mint_price,
+    args.max_tokens,
+    args.base_uri,
+    args.royaltyArtist,
+    args.royaltyBasis
+  );
+  console.log("Waiting for block verifications...");
+  await courseNFTContract.deploymentTransaction().wait(15);
+  let contractAddress = await courseNFTContract.getAddress();
+  console.log(`Contract deployed to ${contractAddress}`);
+  // verify
+  if (
+    // we are on a live testnet and have the correct api key
+    (network.config.chainId === 80001 && process.env.POLYGONSCAN_API_KEY) ||
+    (network.config.chainId === 1115511 && process.env.ETHERSCAN_API_KEY)
+  ) {
+    console.log("Verifying...");
+    await verify(contractAddress, [
+      args.mint_price,
+      args.max_tokens,
+      args.base_uri,
+      args.royaltyArtist,
+      args.royaltyBasis,
+    ]);
+    console.log("Completed.");
+  } else {
+    console.log("No verification available for hardhat network.");
+  }
+  // mint 3
+  // const ipfs = [
+  //   "https://ipfs.io/ipfs/QmdKR2UxXcSLVptr6ggziRG4EKUYNSiS2AFEy3wUAHVqUt",
+  //   "https://ipfs.io/ipfs/QmQnABgoMhgP1t1gyopTS4EmsaPUZ6dwufpq1QWBZg4RyD",
+  //   "https://ipfs.io/ipfs/QmfKLqTkMSNiC3Kc7unLctWkJREWFv4gF8Cknhkdej3snb",
+  // ];
+  // console.log("Minting 3 tokens...");
+  // for (let i = 0; i < 3; i++) {
+  //   const transactionResponse = await courseNFTContract.mintTo(ipfs[i], {
+  //     value: args.mint_price,
+  //   });
+  //   await transactionResponse.wait(3);
+  //   console.log(`Token ${i + 1} completed.`);
+  // }
+}
+
+async function verify(contractAddress, args) {
+  console.log("verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (err) {
+    if (err.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified");
+    } else {
+      console.log(err);
+    }
+  }
+}
+
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
 
 function getTokenId(iter) {
   let s = iter + "";
@@ -336,3 +431,37 @@ function getTokenHash() {
       .join("");
   return h;
 }
+
+/*  for front end
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import axios from "axios";
+import FormData from "form-data";
+import fs from "fs";
+let JWT = process.env.IPFS_JWT;
+let tokenList = [];
+
+export async function startMoralis() {
+  // starts moralis
+  await Moralis.start({
+    apiKey: process.env.MORALIS_API_KEY,
+  });
+}
+
+export async function getNFTs() {
+  // gets all NFTs from a contract
+  const address = "0xb0C3A5152A075190b7112567D8362B114FB0b636"; // <fragile geometry contract //userAddress;
+  const chain = EvmChain.POLYGON;
+  await Moralis.EvmApi.nft
+    //    .getWalletNFTCollections({
+    .getContractNFTs({
+      address,
+      chain,
+    })
+    .then((data) => {
+      tokenList = data.toJSON();
+      //      console.log(tokenList);
+    });
+}
+
+*/
